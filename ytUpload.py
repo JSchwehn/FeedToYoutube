290b55e91ac1feb8e1db3425641f524191b0a623
Jens Schwehn <jschwehn@gmail.com>
2017-01-15 19:19:14 +0100

    WIP

---
+++ ytUpload.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#import sqlite3
+#import feedparser
+#import datetime
+#import sys
+#from feed import Feed
+#from episode import Episode
+#from chapter import Chapter
+#import pprint
+
+
+class YtUploader:
+
+    config = None
+
+    def __init__(self, config):
+        print "Test"
+        self.config = config
+        self.init_db()
+
+
+
